2ff2b94b3bc8b29038c514cb3ba363f48aaae749
SuperFeeder <64524141+maximim10@users.noreply.github.com>
2021-09-18 13:03:58 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include <vector>
 #include <cmath>
 const int N=2;
-const double E=0.001;
+const double E=0.000001;
 using namespace std;
 double func_at_point(vector <double> *point){
-    double c=pow((*point)[0],2)+pow((*point)[1],6);
+    double c=pow(1-(*point)[0],2)+10*pow((*point)[1]-pow((*point)[0],2),2)+2;
     return c;
 };
 void grad_at_point(vector <double> *point, vector <double> *grad){
@@ -36,11 +36,12 @@ void grad2_at_point(vector <double> *point, vector < vector <double> > *grad2){
         }
     }
 };
-double Alg_dop(int a, int b, vector < vector <double> > *matr){                                      // Если a, b равны N - имеем определитель
+double Alg_dop(int a, int b, vector < vector <double> > *matr){                                      // If a, b equals N - this is a determinant
     int M=N-1;
     if (a==N||b==N){ M++; }
     vector < int > perest (M), perest_temp (M);
-    int z,t,d=0,d_temp,k,l;
+    int z,t,k,l;
+    double d=0,d_temp=0;
     for (int i=0;i<M;i++) { perest[i]=i; }
     while (perest[0]<M){
         perest_temp=perest; z=1; d_temp=1; k=0; l=0;
@@ -54,7 +55,7 @@ double Alg_dop(int a, int b, vector < vector <double> > *matr){                 
                         t=perest_temp[j];
                         perest_temp[j]=perest_temp[j+1];
                         perest_temp[j+1]=t;
-                        if (z==1) { z=-1; } else { z=1; }
+                        z*=-1;
                     }
                 }
             }
@@ -65,36 +66,38 @@ double Alg_dop(int a, int b, vector < vector <double> > *matr){                 
         for (int i=0;i<M;i++){
             if (a==i) {k=1;}
             if (perest[i]==b) {l=1;}
-            //cout<<i+k<<" "<<perest[i]+l<<endl;
             d_temp*=(*matr)[i+k][perest[i]+l];
-        }//cout<<endl<<endl;
+        }
         d+=d_temp;
+
         perest[M-1]+=1;
         for (int i=M-1;i>0;i--){
             if (perest[i]==M) { perest[i]=0; perest[i-1]+=1; }
         }
     }
+
     d*=pow(-1,a+b);
     return d;
 }
 void rev(vector < vector <double> > *matr, vector < vector <double> > *matr_rev){
     double d=0;
     d=Alg_dop(N,N,matr);
-    //cout<<111;
+    if (d==0){
+        cout<<"E pobolshe nado, grad2 takoe ne perevarivaet"<<endl;
+    }
     for (int i=0;i<N;i++){
         for (int j=0;j<N;j++){
-
             (*matr_rev)[i][j]=Alg_dop(i,j,matr)/d;
         }
     }
 }
 double Find_Min (vector <double> *point, vector <double> *grad){
-    int f=func_at_point(point);
+    double f=func_at_point(point);
     double Min=func_at_point(point), Min_temp=Min, Min_comp=Min;
     vector <double> point_temp1=(*point), point_temp2=(*point), point_temp, grad_temp (N);
     grad_at_point(point, grad);
-    double a=1,b=1,m=2,l=0.75;
-    for (int b=0;b<1;){
+    double a=1,b=1,m=2,l=0.75,f_prev=f;
+    for (int b=0,d=1;b<1;){
         point_temp=(*point);
         for (int i=0;i<N;i++){
             point_temp[i]=(*point)[i]-a*(*grad)[i];
@@ -103,46 +106,49 @@ double Find_Min (vector <double> *point, vector <double> *grad){
             a*=m;
             continue;
         }
-        for (;;){cout<<a;
+        for (;;d=1){
             a*=l;
+
             point_temp=(*point);
             for (int i=0;i<N;i++){
-                point_temp[i]=(*point)[i]-a*(*grad)[i];
+                point_temp[i]-=a*(*grad)[i];
             }
-            if (func_at_point(&point_temp)<f){
+            f_prev=func_at_point(&point_temp);
+            if (f_prev<f){
                 (*point)=point_temp;
                 b=1;
                 break;
             }
-            if (a<4e-306){
+            for (int i=0;i<N;i++){
+                if ((f_prev-f!=0)||(a*(*grad)[i]>pow(E/4,0.5))) { d=0; }
+            }
+            if (d==1){
+                (*point)=point_temp;
                 b=1;
                 break;
             }
         }
     }
-
+    return func_at_point(point);
 }
 double Find_Min_2(vector <double> *point, vector <double> *grad, vector < vector <double> > *grad2){
+
     grad2_at_point(point, grad2);
     grad_at_point(point, grad);
-    int f=func_at_point(point);
-    vector < vector <double> > grad2_rev=*grad2;
+    double f=func_at_point(point);
+    vector < vector <double> > grad2_rev=(*grad2);
     vector < double > point_temp=(*point);
-    double a=1,b=1,m=2,l=0.75;
+    double a=1,b=1,m=2,l=0.75,f_prev=0;
     rev(grad2, &grad2_rev);
+    for (int i=0;i<N;i++){ for (int j=0;j<N;j++){ cout<<(*grad2)[i][j]<<" ";}cout<<endl;}
     vector < double > move_vec (N);
-    //cout<<(*point)[1]<<endl;
-    //cout<<(*grad2)[0][0]<<" "<<(*grad2)[0][1]<<endl; cout<<(*grad2)[1][0]<<" "<<(*grad2)[1][1]<<endl;
-    //cout<<(grad2_rev)[0][0]<<" "<<(grad2_rev)[0][1]<<endl; cout<<(grad2_rev)[1][0]<<" "<<(grad2_rev)[1][1]<<endl;
     for (int i=0;i<N;i++){
         move_vec[i]=0;
         for (int j=0;j<N;j++){
             move_vec[i]-=grad2_rev[i][j]*(*grad)[j];
-            //cout<<move_vec[i]<<endl;
         }
     }
-    //cout<<move_vec[0]<<" "<<move_vec[1]<<endl;
-    for (int b=0;b<1;){
+    for (int b=0,d=1;b<1;){
         point_temp=(*point);
         for (int i=0;i<N;i++){
             point_temp[i]=(*point)[i]+a*move_vec[i];
@@ -151,36 +157,29 @@ double Find_Min_2(vector <double> *point, vector <double> *grad, vector < vector
             a*=m;
             continue;
         }
-        for (;;){
+        for (;;d=1){
             a*=l;
             point_temp=(*point);
             for (int i=0;i<N;i++){
                 point_temp[i]=(*point)[i]+a*move_vec[i];
             }
-            if (func_at_point(&point_temp)<f){
+            f_prev=func_at_point(&point_temp);
+            if (f_prev<f){
                 (*point)=point_temp;
                 b=1;
                 break;
             }
-            if (a<4e-306){
+            for (int i=0;i<N;i++){
+                if ((f_prev!=f)||(a*(*grad)[i]>E/4)) { d=0;}
+            }
+            if (d==1){
+                (*point)=point_temp;
                 b=1;
                 break;
             }
         }
     }
     return func_at_point(point);
-
-    for (int i=0;i<N;i++){
-        for (int j=0;j<N;j++){
-
-        }
-    }
-    vector <double> gr2_m_gr (N);
-    for (int i=0;i<N;i++){
-        for (int j=0;j<N;j++){
-            a+=(*grad2)[i][j];
-        }
-    }
 }
 int main()
 {
@@ -190,31 +189,15 @@ int main()
     vector <double> grad (N);
     vector <double> point_temp=point;
     vector < vector <double> > grad2 (N);
-    //vector < vector <double> > matr (N);
-    //vector < vector <double> > matr_rev (N);
-    //for (int i=0;i<N;i++) { for (int j=0;j<N;j++) {matr[i].push_back(0);} }
-    //for (int i=0;i<N;i++) { for (int j=0;j<N;j++) {matr_rev[i].push_back(0);} }
-
-    //matr[0][0]=2; matr[1][1]=2; matr[0][1]=2; matr[1][0]=0;
-    //rev(&matr, &matr_rev);
-    //for (int i=0;i<N;i++){
-        //for (int j=0;j<N;j++){
-    //        cout<<matr_rev[i][j]<<" ";
-    //    }
-    //    cout<<endl;
-    //}
     for (int i=0;i<N;i++) { for (int j=0;j<N;j++) {grad2[i].push_back(0);} }
     double a=func_at_point(&point),n=0;
     double b=a,c,d;
-
-    cout<<a<<endl;
     for(;;){
         point_temp=point;
         b=Find_Min(&point, &grad);
         cout<<b<<" at step "<<++n<<" at point ("; for (int i=0;i<N;i++) { cout<<point[i]<<", "; } cout<<")"<<endl;
         c=0; for (int i=0;i<N;i++) {c+=pow(point[i]-point_temp[i],2);} c=pow(c,0.5);
         grad_at_point(&point,&grad);
-        cout<<grad[0]<<" "<<grad[1]<<endl;
         d=0; for (int i=0;i<N;i++) {d+=pow(grad[i],2);} d=pow(d,0.5);
         if ((b-a<pow(E,0.5))&&(c<pow(E,0.5))&&(d<pow(E,0.5))) { break; }
         a=b;
